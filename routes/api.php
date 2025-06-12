43f4b672f7c0a67782a77ee943fcfb9877bf8465
dimdimcraft <dimdimcraft.id@gmail.com>
2025-06-12 07:45:53 +0700

    open all route

--- routes/api.php
+++ routes/api.php
@@ -1,36 +1,53 @@
 <?php
 
+// use App\Http\Controllers\AuthorController;
+// use App\Http\Controllers\BookController;
+// use App\Http\Controllers\GenreController;
+// use App\Http\Controllers\TransactionController;
+// use App\Models\Transaction;
+// use Illuminate\Http\Request;
+// use Illuminate\Support\Facades\Route;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+// // akses umum
+// Route::post('/register', [\App\Http\Controllers\AuthController::class, 'register']);
+// Route::post('/login', [\App\Http\Controllers\AuthController::class, 'login']);
+// Route::post('/logout', [\App\Http\Controllers\AuthController::class, 'logout'])->middleware('auth:api');
+
+// Route::middleware('auth:api')->group(function () {
+//     Route::apiResource('genres', GenreController::class)->only(['store','update', 'show']);
+//     Route::apiResource('authors', AuthorController::class)->only(['store','update', 'show']);
+//     Route::apiResource('transactions', TransactionController::class)->only(['store','update', 'show']);
+//     Route::apiResource('books', BookController::class)->only(['store','update', 'show']);
+
+//     // hanya admin
+//     Route::middleware('role:admin')->group(function () {
+//         Route::apiResource('genres', GenreController::class);
+//         Route::apiResource('authors', AuthorController::class);
+//         Route::apiResource('books', BookController::class);
+//         Route::apiResource('transactions', TransactionController::class);
+//     });
+// });
+
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\GenreController;
 use App\Http\Controllers\TransactionController;
-use App\Models\Transaction;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-// akses umum
+// akses umum (semua endpoint public)
 Route::post('/register', [\App\Http\Controllers\AuthController::class, 'register']);
 Route::post('/login', [\App\Http\Controllers\AuthController::class, 'login']);
-Route::post('/logout', [\App\Http\Controllers\AuthController::class, 'logout'])->middleware('auth:api');
+Route::post('/logout', [\App\Http\Controllers\AuthController::class, 'logout']);
 
-Route::middleware('auth:api')->group(function () {
-    Route::apiResource('genres', GenreController::class)->only(['store','update', 'show']);
-    Route::apiResource('authors', AuthorController::class)->only(['store','update', 'show']);
-    Route::apiResource('transactions', TransactionController::class)->only(['store','update', 'show']);
-    Route::apiResource('books', BookController::class)->only(['store','update', 'show']);
-
-    // hanya admin
-    Route::middleware('role:admin')->group(function () {
-        Route::apiResource('genres', GenreController::class)->only(['index', 'destroy']);
-        Route::apiResource('authors', AuthorController::class)->only(['index', 'destroy']);
-        Route::apiResource('books', BookController::class)->only(['index', 'destroy']);
-        Route::apiResource('transactions', TransactionController::class)->only(['index', 'destroy']);
-    });
-});
+Route::apiResource('genres', GenreController::class);
+Route::apiResource('authors', AuthorController::class);
+Route::apiResource('transactions', TransactionController::class);
+Route::apiResource('books', BookController::class);
 
 
 
